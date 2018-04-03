464ff2ad5cce7960f5ea91a25bf75ddf0a24db9f
erikseyti <erikseyti@gmail.com>
2018-04-03 11:13:25 -0300

    Projeto Conversor Moeda

---
+++ ConversorMoeda/app/src/main/java/com/example/aluno/conversormoeda/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.aluno.conversormoeda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList <String> conversao = new ArrayList<String>();
+        conversao.add("Real");
+        conversao.add("Dolar");
+        conversao.add("Iene");
+        conversao.add("Euro");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerInicial);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+
+    }
+
+}
