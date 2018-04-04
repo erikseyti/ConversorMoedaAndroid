587ed72db61307863c05bcf003023384cbff0f5c
erikseyti <erikseyti@gmail.com>
2018-04-04 19:49:16 -0300

    Adicionado tambem o array de moedas para o Spinnerfinal

--- ConversorMoeda/app/src/main/java/com/example/aluno/conversormoeda/MainActivity.java
+++ ConversorMoeda/app/src/main/java/com/example/aluno/conversormoeda/MainActivity.java
@@ -15,19 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList <String> conversao = new ArrayList<String>();
-        conversao.add("Real");
-        conversao.add("Dolar");
-        conversao.add("Iene");
-        conversao.add("Euro");
 
         Spinner spinner = (Spinner) findViewById(R.id.spinnerInicial);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.moedas_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinnerFinal);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.moedas_array,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
 
 
 
